4bebe186caf1e39de3c7527bdb57f679a34d3cd8
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-07-08 12:22:13 +0000

    Add

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,9 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
+
   end
 
   test "should be valid" do
@@ -31,5 +33,14 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end

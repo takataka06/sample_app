a15353878e3fcbd6f5f5d0c6996e07d909943c94
takataka06 <206020787+takataka06@users.noreply.github.com>
2025-07-09 18:53:04 +0000

    Add create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   def show
     @user = User.find(params[:id])
-   
   end
-  
-end 
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user  # ユーザーの作成に成功した場合、showアクションにリダイレクトします
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
